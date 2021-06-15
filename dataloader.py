Change input and reconstruction CNNs. Fix image masking. Misc updates.

dataloader.py
@@ -27,7 +27,7 @@ class Dataset(torch.utils.data.Dataset):
         img = resize_image(img)
         img = normalize_image(img)
         img = torch.from_numpy(np.ascontiguousarray(img))
-        masked_img, mask = mask_random_crop(img)
+        masked_img, mask = mask_random_crop(img.detach().clone())
         net_input = torch.cat([masked_img, mask], dim=2)
 
         return torch.movedim(net_input,2,0), torch.movedim(img,2,0)

glom.py
@@ -59,6 +59,7 @@ class GLOM(nn.Module):
         probs = scipy.stats.norm.cdf(dists+1/self.attention_std)-scipy.stats.norm.cdf(dists)
         np.fill_diagonal(probs,0.)
         self.probs = torch.tensor(np.reshape(probs,(128,128,128,128)),device='cuda')
+        self.probs[self.probs < 0.] = 0.
 
         # Threshold used to determine when to stop updating the embeddings
         self.delta_thresh = 10.
@@ -131,12 +132,14 @@ class GLOM(nn.Module):
 
     def build_input_cnn(self):
         # Input CNN used to initialize the embeddings at each of the levels (see pg 13: 3.5 The Visual Input)
-        cnn_channels = [4,16,32,self.min_emb_size]
+        cnn_channels = [4,self.min_emb_size//4,self.min_emb_size//2,self.min_emb_size]
         cnn_layers = []
         for l in range(self.num_input_layers):
-            cnn_layers.append(('cnn_conv_inp{}'.format(l), nn.Conv2d(cnn_channels[l],cnn_channels[l+1],kernel_size=3,stride=2,padding=1)))
+            cnn_layers.append(('cnn_conv_inp_s1{}'.format(l), nn.Conv2d(cnn_channels[l],cnn_channels[l+1],kernel_size=3,stride=1,padding=1)))
+            cnn_layers.append(('cnn_act_inp_s1{}'.format(l), nn.Hardswish(inplace=True)))
+            cnn_layers.append(('cnn_conv_inp_s2{}'.format(l), nn.Conv2d(cnn_channels[l+1],cnn_channels[l+1],kernel_size=3,stride=2,padding=1)))
             if l < self.num_input_layers-1:
-                cnn_layers.append(('cnn_act_inp{}'.format(l), nn.Hardswish(inplace=True)))
+                cnn_layers.append(('cnn_act_inp_s2{}'.format(l), nn.Hardswish(inplace=True)))
 
         #for l in range(self.num_input_layers, self.num_input_layers+self.num_levels):
         #    cnn_layers['cnn_conv_lev{}'.format(l)] = nn.Conv2d(cnn_channels[l-1],cnn_channels[l],kernel_size=3,
@@ -147,12 +150,17 @@ class GLOM(nn.Module):
 
     def build_reconstruction_net(self):
         # CNN used to reconstruct the input image (and the missing pixels) using the embeddings from the bottom level.
-        reconst_chans = [self.min_emb_size,128,256,192]
+        reconst_chans = [self.min_emb_size,int(self.min_emb_size*1.5),self.min_emb_size,self.min_emb_size//2]
         reconst_layers = []
         for l in range(self.num_reconst):
-            reconst_layers.append(('reconst_dense{}'.format(l), nn.Conv2d(reconst_chans[l],reconst_chans[l+1],kernel_size=1,stride=1)))
-            if l < self.num_reconst-1:
-                reconst_layers.append(('reconst_act{}'.format(l), nn.Hardswish(inplace=True)))
+            reconst_layers.append(('reconst_conv{}'.format(l), nn.Conv2d(reconst_chans[l],reconst_chans[l+1],kernel_size=3,stride=1,padding=1)))
+            reconst_layers.append(('reconst_conv_act{}'.format(l), nn.Hardswish(inplace=True)))
+            reconst_layers.append(('reconst_trans{}'.format(l), nn.ConvTranspose2d(reconst_chans[l+1],reconst_chans[l+1],kernel_size=4,stride=2,padding=1)))
+            reconst_layers.append(('reconst_act{}'.format(l), nn.Hardswish(inplace=True)))
+
+        reconst_layers.append(('reconst_conv_out', nn.Conv2d(self.min_emb_size//2,self.min_emb_size//2,kernel_size=3,stride=1,padding=1)))
+        reconst_layers.append(('reconst_conv_act_out', nn.Hardswish(inplace=True)))
+        reconst_layers.append(('reconst_final', nn.Conv2d(self.min_emb_size//2,3,kernel_size=3,stride=1,padding=1)))
 
         return nn.Sequential(OrderedDict(reconst_layers))
 
@@ -214,12 +222,13 @@ class GLOM(nn.Module):
         if FLAGS.add_predictor:
             preds = self.pred_net[level](preds)
 
-        if FLAGS.l2_normalize:
-            preds = F.normalize(preds, dim=1)
-            level_embds = F.normalize(level_embds, dim=1)
+        #if FLAGS.l2_normalize:
+        #    preds = F.normalize(preds, dim=1)
+        #    level_embds = F.normalize(level_embds, dim=1)
 
-        dot_prod = (preds*level_embds).sum(1)
-        return dot_prod
+        #dot_prod = (preds*level_embds).sum(1)
+        #return dot_prod
+        return F.mse_loss(preds,level_embds)
 
     def update_embeddings(self, level_embds):
         level_deltas = []
@@ -230,32 +239,38 @@ class GLOM(nn.Module):
             top_down = self.top_down(level_embds[level+1],level) if level < self.num_levels-1 else self.zero_tensor
             attention_embd = self.attend_to_level(level_embds[level])
             prev_timestep = level_embds[level]
-            #print(bottom_up.mean(),top_down.mean(),attention_embd.mean(),prev_timestep.mean())
+            #if level in [0,1,3]:
+            #    #print(level,bottom_up.norm(dim=1).mean(),top_down.norm(dim=1).mean(),attention_embd.norm(dim=1).mean(),prev_timestep.norm(dim=1).mean())
+            #    print(level, prev_timestep[0,:,3,3])
 
             # The embedding at each timestep is the average of 4 contributions (see pg. 3)
-            level_embds[level] = (bottom_up+top_down+attention_embd+prev_timestep)/self.num_contribs[level]
+            level_embds[level] = (0.2*bottom_up+0.2*top_down+attention_embd+2.6*prev_timestep)/4.
 
             # Calculate regularization loss (See bottom of pg 3 and Section 7: Learning Islands)
             if level > 0:
-                bu_loss.append((1.-self.similarity(level_embds[level].detach(), bottom_up, level)).mean())
+                bu_loss.append(self.similarity(level_embds[level].detach(), bottom_up, level))
             if level < self.num_levels-1:
-                td_loss.append((1.-self.similarity(level_embds[level].detach(), top_down, level)).mean())
+                td_loss.append(self.similarity(level_embds[level].detach(), top_down, level))
 
             # level_deltas measures the magnitude of the change in the embeddings between timesteps; when the change is less than a 
             # certain threshold the embedding updates are stopped.
-            #level_deltas.append(torch.norm(level_embds[level]-prev_timestep,dim=1).mean())
+            with torch.no_grad():
+                level_deltas.append(torch.norm(level_embds[level]-prev_timestep,dim=1).mean())
 
         return level_embds, level_deltas, bu_loss, td_loss
 
     def forward(self, img):
         batch_size,chans,height,width = img.shape
-        print(img.shape)
+        #print(img.shape)
         embd_input = self.input_cnn(img)
         level_embds = [embd_input]
+        #print(level_embds[-1].norm(dim=1).mean())
         for level in range(1,self.num_levels):
             level_embds.append(self.bottom_up_net[level-1](level_embds[-1]))
+            #print(level_embds[-1].norm(dim=1).mean())
 
         total_bu_loss, total_td_loss = 0.,0.
+        delta_log = []
         # Keep on updating embeddings until they settle on constant value.
         for t in range(FLAGS.timesteps):
             level_embds, deltas, bu_loss, td_loss = self.update_embeddings(level_embds)
@@ -264,10 +279,14 @@ class GLOM(nn.Module):
             #print(sum(deltas))
             #if sum(deltas) < self.delta_thresh:
             #    break
+            if t in [0,1,2,5,9]:
+                delta_log.append((deltas[0],deltas[-1]))
 
         reconst_img = self.reconstruction_net(level_embds[0]) # N,192,32,32
+        total_bu_loss = total_td_loss = 0.
+        delta_log = []
         _,_,map_h,map_w = reconst_img.shape
-        reconst_img = reconst_img.movedim(1,3).reshape(batch_size,map_h,map_w,8,8,3).movedim(2,3).reshape(batch_size,map_h*8,map_w*8,3).movedim(3,1)
-        return reconst_img, total_bu_loss, total_td_loss
+        #reconst_img = reconst_img.movedim(1,3).reshape(batch_size,map_h,map_w,8,8,3).movedim(2,3).reshape(batch_size,map_h*8,map_w*8,3).movedim(3,1)
+        return reconst_img, total_bu_loss, total_td_loss, delta_log
 
 

train.py
@@ -16,8 +16,8 @@ FLAGS = flags.FLAGS
 flags.DEFINE_string('exp','test','')
 flags.DEFINE_integer('batch_size',1,'')
 flags.DEFINE_integer('num_workers',8,'')
-flags.DEFINE_integer('min_crop_size',8,'Minimum size of cropped region')
-flags.DEFINE_integer('max_crop_size',24,'Maximum size of cropped region')
+flags.DEFINE_integer('min_crop_size',24,'Minimum size of cropped region')
+flags.DEFINE_integer('max_crop_size',64,'Maximum size of cropped region')
 flags.DEFINE_float('masked_fraction',0.2,'Fraction of input image that is masked')
 flags.DEFINE_bool('joint_patch_reconstruction',False,'Whether to reconstruct each image patch separately or jointly')
 
@@ -27,7 +27,7 @@ flags.DEFINE_bool('l2_normalize',True,'L2 normalize embeddings before calculatin
 flags.DEFINE_string('layer_norm','bu','bu,bu_and_td,out,none')
 
 flags.DEFINE_float('lr',0.0003,'Learning Rate')
-flags.DEFINE_float('reg_coeff',0.1,'Regularization coefficient used for regularization loss')
+flags.DEFINE_float('reg_coeff',1.,'Regularization coefficient used for regularization loss')
 
 flags.DEFINE_integer('num_levels',5,'Number of levels in part-whole hierarchy')
 flags.DEFINE_integer('timesteps',10,'Number of timesteps')
@@ -56,14 +56,20 @@ def main(argv):
     print("Num train images:",len(train_images))
     #print("Num val images:",len(val_images))
 
+    IMAGENET_DEFAULT_MEAN = (255*0.485, 255*0.456, 255*0.406)
+    IMAGENET_DEFAULT_STD = (255*0.229, 255*0.224, 255*0.225)
+
     training_set = Dataset(train_images)
-    training_generator = torch.utils.data.DataLoader(training_set, batch_size=FLAGS.batch_size, shuffle=True)
+    training_generator = torch.utils.data.DataLoader(training_set, batch_size=FLAGS.batch_size, shuffle=True, num_workers=FLAGS.num_workers)
 
     #validation_set = Dataset(val_images)
     #validation_generator = torch.utils.data.DataLoader(validation_set, batch_size=FLAGS.batch_size, shuffle=True)
 
     model = GLOM(num_levels=FLAGS.num_levels, min_emb_size=FLAGS.min_emb_size, patch_size=(FLAGS.min_patch_size,FLAGS.max_patch_size), bottom_up_layers=FLAGS.bottom_up_layers, 
                 top_down_layers=FLAGS.top_down_layers, num_input_layers=FLAGS.input_cnn_depth, num_reconst=FLAGS.num_reconst)
+
+    model.input_cnn.load_state_dict(torch.load('weights/input_cnn_{}.pt'.format(FLAGS.min_emb_size)))
+    model.reconstruction_net.load_state_dict(torch.load('weights/reconstruction_net_{}.pt'.format(FLAGS.min_emb_size)))
 
     optimizer = torch.optim.Adam(params=model.parameters(),lr=FLAGS.lr)
 
@@ -73,31 +79,59 @@ def main(argv):
     model.train()
 
     print((datetime.datetime.now()-start).total_seconds())
-
+    total_loss = 0.
     train_iter = 0
-    for masked_load, target_load in training_generator:
-        # Set optimzer gradients to zero
-        optimizer.zero_grad()
+    for epoch in range(10000):
+        
+        for masked_load, target_load in training_generator:
+            # Set optimzer gradients to zero
+            optimizer.zero_grad()
 
-        masked_image = masked_load.to('cuda')
-        target_image = target_load.to('cuda')
+            masked_image = masked_load.to('cuda')
+            target_image = target_load.to('cuda')
 
-        reconstructed_image, bottom_up_loss, top_down_loss = model(masked_image)
-        reconstruction_loss = loss_func(target_image,reconstructed_image)
-        final_loss = reconstruction_loss + FLAGS.reg_coeff*(bottom_up_loss+top_down_loss)
+            reconstructed_image, bottom_up_loss, top_down_loss, delta_log = model(masked_image)
+            reconstruction_loss = loss_func(target_image,reconstructed_image)
+            final_loss = reconstruction_loss + FLAGS.reg_coeff*(bottom_up_loss+top_down_loss)
 
-        # Calculate gradients of the weights
-        final_loss.backward()
+            # Calculate gradients of the weights
+            final_loss.backward()
 
-        # Update the weights
-        optimizer.step()
+            # Update the weights
+            optimizer.step()
 
-        train_iter += 1
-        log_dict = {"Train Iteration":train_iter, "Final Loss": final_loss, "Reconstruction Loss":reconstruction_loss, 
-                    "Bottom-Up Loss": bottom_up_loss, "Top-Down Loss":top_down_loss}
-        print(log_dict)
-        wandb.log(log_dict)
+            train_iter += 1
+            log_dict = {"Train Iteration":train_iter, "Final Loss": final_loss, "Reconstruction Loss":reconstruction_loss, 
+                        "Bottom-Up Loss": bottom_up_loss, "Top-Down Loss":top_down_loss}
 
+            if train_iter % 100 == 0:
+                print(log_dict)
+                #print(total_loss/100)
+                #total_loss = 0.
+
+            for ts,ts_delta in zip([0,1,2,5,9],delta_log):
+                log_dict['delta_l1_t{}'.format(ts)] = ts_delta[0]
+                log_dict['delta_l5_t{}'.format(ts)] = ts_delta[1]
+
+            wandb.log(log_dict)
+
+            if train_iter > 10000 and train_iter%100==0:
+                imshow = reconstructed_image[0].detach().movedim(0,2).cpu().numpy() * IMAGENET_DEFAULT_STD + IMAGENET_DEFAULT_MEAN
+                imshow = np.clip(imshow,0,255)
+                imshow = imshow.astype(np.uint8)
+                targ = target_image[0].detach().movedim(0,2).cpu().numpy() * IMAGENET_DEFAULT_STD + IMAGENET_DEFAULT_MEAN
+                targ = targ.astype(np.uint8)
+                cv2.imshow('pred',imshow)
+                cv2.imshow('target',targ)
+                key = cv2.waitKey(0)
+                if key==27:
+                    cv2.destroyAllWindows()
+                    exit()
+
+                #torch.save(model.input_cnn.state_dict(),'weights/input_cnn_{}.pt'.format(FLAGS.min_emb_size))
+                #torch.save(model.reconstruction_net.state_dict(),'weights/reconstruction_net_{}.pt'.format(FLAGS.min_emb_size))
+
+        
 
 if __name__ == '__main__':
     #torch.multiprocessing.set_start_method('spawn', force=True)

utils.py
@@ -12,10 +12,11 @@ IMAGENET_DEFAULT_STD = (255*0.229, 255*0.224, 255*0.225)
 
 def resize_image(image):
     height, width, _ = image.shape
-    new_height = min(1024, height - height%FLAGS.max_patch_size)
-    new_width = min(1024, width - width%FLAGS.max_patch_size)
+    new_height = min(384, height - height%FLAGS.max_patch_size)
+    new_width = min(384, width - width%FLAGS.max_patch_size)
+    lu = (np.random.randint(0,max(1,image.shape[0]-new_height)), np.random.randint(0,max(1,image.shape[1]-new_width)))
 
-    return image[:new_height, :new_width]
+    return image[lu[0]:lu[0]+new_height, lu[1]:lu[1]+new_width]
 
 def normalize_image(image):
     image = image.astype(np.float32)
@@ -32,7 +33,7 @@ def mask_random_crop(image):
     while mask.sum() < FLAGS.masked_fraction*img_area:
         lu = np.random.randint([0, 0], [image.shape[0]-FLAGS.max_crop_size, image.shape[1]-FLAGS.max_crop_size])
         wh = np.random.randint([FLAGS.min_crop_size, FLAGS.min_crop_size], [FLAGS.max_crop_size, FLAGS.max_crop_size])
-        mask[lu[0]:lu[1], lu[0]+wh[0]:lu[1]+wh[1]] = 1.
+        mask[lu[0]:lu[0]+wh[0], lu[1]:lu[1]+wh[1]] = 1.
 
     image[mask.bool()] = 0.
     return image, mask.view(mask.shape[0],mask.shape[1],1)