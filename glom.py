Fix BU bug. Add gradient clipping.

glom.py
@@ -325,9 +325,9 @@ class GLOM(nn.Module):
         td_loss = []
         for level in range(self.num_levels + min(FLAGS.timesteps-ts-self.num_levels,0)):
             if FLAGS.sg_bu:
-                bottom_up = self.out_norm[level](self.bottom_up_net[level-1](level_embds[level-1].detach())) if level > 0 else self.zero_tensor
+                bottom_up = self.out_norm[level](self.bottom_up_net[level-1](prev_timestep.detach())) if level > 0 else self.zero_tensor
             else:
-                bottom_up = self.out_norm[level](self.bottom_up_net[level-1](level_embds[level-1])) if level > 0 else self.zero_tensor
+                bottom_up = self.out_norm[level](self.bottom_up_net[level-1](prev_timestep)) if level > 0 else self.zero_tensor
             if FLAGS.sg_td:
                 top_down = self.out_norm[level](self.top_down(level_embds[level+1].detach(), level)) if level < self.num_levels-1 else self.zero_tensor
             else:
@@ -380,53 +380,56 @@ class GLOM(nn.Module):
             embd_input = self.out_norm[0](self.input_cnn(img))
 
         level_embds = [embd_input.clone()]
-        for level in range(1,self.num_levels):
-            level_embds.append(self.out_norm[level](self.bottom_up_net[level-1](level_embds[-1])))
-
-        if not FLAGS.att_to_masks:
-            mask = img[0,3,:,:]
-            for l in list(self.probs.keys()):
-                patch_size = self.granularity[l]
-                lev_h,lev_w = h//patch_size, w//patch_size
-                lev_mask = mask.reshape(lev_h,patch_size,lev_w,patch_size).max(3)[0].max(1)[0].reshape(1,1,lev_h,lev_w)
-                self.probs[l] = self.unmasked_probs[l][:lev_h,:lev_w,:lev_h,:lev_w]*(1-lev_mask)
-
-        total_bu_loss, total_td_loss = 0.,0.
-        delta_log = []
-        norms_log = []
-        bu_log = []
-        td_log = []
-        # Keep on updating embeddings for t timesteps
-        for t in range(FLAGS.timesteps):
-            level_embds, deltas, norms, bu_loss, td_loss = self.update_embeddings(level_embds, t, embd_input)
-            if t >= FLAGS.ts_reg:
-                total_bu_loss += sum(bu_loss)/max(1.,len(bu_loss))
-                total_td_loss += sum(td_loss)/max(1.,len(td_loss))
-
-            if t in [0,1,4,7]:
-                delta_log.append((deltas[0],deltas[2],deltas[4]))
-                norms_log.append((norms[1],norms[2],norms[3]))
-            if t in [10,11]:
-                delta_log.append(deltas[0])
-
-            if FLAGS.ts_reg <= t <= 7:
-                bu_log.append((bu_loss[0],bu_loss[2],bu_loss[-1],t))
-                td_log.append((td_loss[0],td_loss[2],td_loss[-1],t))
-
-
-        if FLAGS.all_lev_reconst:
-            reconst_embd = level_embds[0]
+        if not FLAGS.only_reconst:
             for level in range(1,self.num_levels):
-                inp_embd = level_embds[level]
-                for td_lev in range(level,0,-1):
-                    inp_embd = self.out_norm[td_lev-1](self.top_down(inp_embd, td_lev-1))
-                reconst_embd = reconst_embd + inp_embd
-            reconst_embd = reconst_embd/self.num_levels
-            reconst_img = self.reconstruct_image(reconst_embd)
+                level_embds.append(self.out_norm[level](self.bottom_up_net[level-1](level_embds[-1])))
+
+            if not FLAGS.att_to_masks:
+                mask = img[0,3,:,:]
+                for l in list(self.probs.keys()):
+                    patch_size = self.granularity[l]
+                    lev_h,lev_w = h//patch_size, w//patch_size
+                    lev_mask = mask.reshape(lev_h,patch_size,lev_w,patch_size).max(3)[0].max(1)[0].reshape(1,1,lev_h,lev_w)
+                    self.probs[l] = self.unmasked_probs[l][:lev_h,:lev_w,:lev_h,:lev_w]*(1-lev_mask)
+
+            total_bu_loss, total_td_loss = 0.,0.
+            delta_log = []
+            norms_log = []
+            bu_log = []
+            td_log = []
+            # Keep on updating embeddings for t timesteps
+            for t in range(FLAGS.timesteps):
+                level_embds, deltas, norms, bu_loss, td_loss = self.update_embeddings(level_embds, t, embd_input)
+                if t >= FLAGS.ts_reg:
+                    total_bu_loss += sum(bu_loss)/max(1.,len(bu_loss))
+                    total_td_loss += sum(td_loss)/max(1.,len(td_loss))
+
+                if t in [0,1,4,7]:
+                    delta_log.append((deltas[0],deltas[2],deltas[4]))
+                    norms_log.append((norms[1],norms[2],norms[3]))
+                if t in [10,11]:
+                    delta_log.append(deltas[0])
+
+                if FLAGS.ts_reg <= t <= 7:
+                    bu_log.append((bu_loss[0],bu_loss[2],bu_loss[-1],t))
+                    td_log.append((td_loss[0],td_loss[2],td_loss[-1],t))
+
+
+            if FLAGS.all_lev_reconst:
+                reconst_embd = level_embds[0]
+                for level in range(1,self.num_levels):
+                    inp_embd = level_embds[level]
+                    for td_lev in range(level,0,-1):
+                        inp_embd = self.out_norm[td_lev-1](self.top_down(inp_embd, td_lev-1))
+                    reconst_embd = reconst_embd + inp_embd
+                reconst_embd = reconst_embd/self.num_levels
+                reconst_img = self.reconstruct_image(reconst_embd)
+            else:
+                reconst_img = self.reconstruct_image(level_embds[0])
+
+            return reconst_img, total_bu_loss, total_td_loss, delta_log, norms_log, bu_log, td_log, level_embds
         else:
             reconst_img = self.reconstruct_image(level_embds[0])
-
-        return reconst_img, total_bu_loss, total_td_loss, delta_log, norms_log, bu_log, td_log, level_embds
-        #return reconst_img, 0., 0., [], [], [], [], 0.
+            return reconst_img, 0., 0., [], [], [], [], level_embds
 
 

train.py
@@ -4,6 +4,8 @@ import torch
 import glob
 import datetime
 
+from nfnets.agc import AGC
+
 from glom import GLOM
 from dataloader import Dataset
 
@@ -17,28 +19,31 @@ FLAGS = flags.FLAGS
 
 flags.DEFINE_string('exp','test','')
 flags.DEFINE_integer('batch_size',1,'')
+flags.DEFINE_bool('use_agc',False,'')
+flags.DEFINE_float('clip_grad',15.,'')
 flags.DEFINE_integer('num_workers',8,'')
 flags.DEFINE_integer('min_crop_size',24,'Minimum size of cropped region')
 flags.DEFINE_integer('max_crop_size',64,'Maximum size of cropped region')
 flags.DEFINE_float('masked_fraction',0.2,'Fraction of input image that is masked')
 flags.DEFINE_bool('all_lev_reconst',False,'')
+flags.DEFINE_bool('only_reconst',False,'')
 
 # Contrastive learning flags
 flags.DEFINE_bool('add_predictor',True,'Whether to add predictor MLP')
 flags.DEFINE_bool('sep_preds', False, '')
 flags.DEFINE_bool('symm_pred', False, '')
 flags.DEFINE_bool('sg_td',False,'')
-flags.DEFINE_bool('sg_bu', True, '')
+flags.DEFINE_bool('sg_bu', False, '')
 flags.DEFINE_bool('l2_normalize',False,'L2 normalize embeddings before calculating contrastive loss.')
 flags.DEFINE_string('layer_norm','out','bu,bu_and_td,out,none')
 
 flags.DEFINE_float('lr',0.0003,'Learning Rate')
-flags.DEFINE_float('mask_coeff',1.,'')
-flags.DEFINE_float('reg_coeff',3.,'Regularization coefficient used for regularization loss')
+flags.DEFINE_float('mask_coeff',3.,'')
+flags.DEFINE_float('reg_coeff',1.,'Regularization coefficient used for regularization loss')
 flags.DEFINE_float('bu_coeff',1.,'Bottom-Up Loss Coefficient')
 flags.DEFINE_bool('linear_input',True,'')
 flags.DEFINE_bool('linear_reconst',True,'')
-flags.DEFINE_bool('train_input_cnn',True,'')
+flags.DEFINE_bool('train_input_cnn',False,'')
 
 flags.DEFINE_integer('num_levels',5,'Number of levels in part-whole hierarchy')
 flags.DEFINE_string('granularity','8,8,8,16,32','')
@@ -94,9 +99,16 @@ def main(argv):
 
     model.input_cnn.load_state_dict(torch.load('weights/input_cnn_{}_{}.pt'.format(FLAGS.linear_input,FLAGS.embd_mult*granularity[0])))
     model.reconstruction_net.load_state_dict(torch.load('weights/reconstruction_net_{}_{}.pt'.format(FLAGS.linear_reconst,FLAGS.embd_mult*granularity[0])))
-    #model.load_state_dict(torch.load('weights/1July2.pt'))
+    #model.load_state_dict(torch.load('weights/5July1.pt'))
 
-    optimizer = torch.optim.Adam(params=model.parameters(),lr=FLAGS.lr)
+    if FLAGS.use_agc:
+        optimizer = torch.optim.SGD(params=model.parameters(),lr=FLAGS.lr)
+        optimizer = AGC(model.parameters(), optimizer, clipping=0.08)
+    else:
+        optimizer = torch.optim.AdamW(params=model.parameters(), lr=FLAGS.lr)
+
+    for par in model.reconstruction_net.parameters():
+        par.requires_grad = False
 
     loss_func = torch.nn.MSELoss(reduction='none')
 
@@ -128,12 +140,16 @@ def main(argv):
             # Calculate gradients of the weights
             final_loss.backward()
 
-            # Update the weights
-            optimizer.step()
-
             train_iter += 1
             log_dict = {"Train Iteration":train_iter, "Final Loss": final_loss, "Masked Loss":masked_loss, "Reconstruction Loss": unmasked_loss,
                         "Bottom-Up Loss": bottom_up_loss, "Top-Down Loss":top_down_loss}
+
+            # Update the weights
+            if FLAGS.clip_grad > 0. and not FLAGS.use_agc:
+                log_dict['bu_grad_norm'] = torch.nn.utils.clip_grad_norm_(model.bottom_up_net.parameters(), FLAGS.clip_grad)
+                log_dict['td_grad_norm'] = torch.nn.utils.clip_grad_norm_(model.top_down_net.parameters(), FLAGS.clip_grad)
+
+            optimizer.step()
 
             if train_iter % 100 == 0:
                 print(log_dict)